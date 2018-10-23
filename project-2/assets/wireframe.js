68dbd8ca3c293798bf318b40b4b46303b4f692d1
Cait Brooks <cbrooks5@nd.edu>
2018-10-23 13:02:08 -0400

    project 2
    everything so far

---
+++ project-2/assets/wireframe.js
@@ -0,0 +1,27 @@
+function cloud() {
+    var x = document.getElementById("mycloud");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function sun() {
+    var x = document.getElementById("mysun");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+function wave() {
+    var w = document.getElementById("mywave");
+    if (w.style.display === "none") {
+        w.style.display = "block";
+    } else {
+        w.style.display = "none";
+    }
+}
