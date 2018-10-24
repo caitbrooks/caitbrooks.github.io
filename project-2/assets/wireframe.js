13c18ba59123bf478459421caa888357eed49b10
Cait Brooks <cbrooks5@nd.edu>
2018-10-24 12:53:00 -0400

    project 2 web
    make website

--- project-2/assets/wireframe.js
+++ project-2/assets/wireframe.js
@@ -8,11 +8,11 @@ function cloud() {
 }
 
 function sun() {
-    var x = document.getElementById("mysun");
-    if (x.style.display === "none") {
-        x.style.display = "block";
+    var s = document.getElementById("mysun");
+    if (s.style.display === "none") {
+        s.style.display = "block";
     } else {
-        x.style.display = "none";
+        s.style.display = "none";
     }
 }
 
@@ -23,5 +23,41 @@ function wave() {
         w.style.display = "block";
     } else {
         w.style.display = "none";
+    }
+}
+
+function plant() {
+    var p = document.getElementById("myplant");
+    if (p.style.display === "none") {
+        p.style.display = "block";
+    } else {
+        p.style.display = "none";
+    }
+}
+
+function towel() {
+    var t = document.getElementById("mytowel");
+    if (t.style.display === "none") {
+        t.style.display = "block";
+    } else {
+        t.style.display = "none";
+    }
+}
+
+function castle() {
+    var c = document.getElementById("mycastle");
+    if (c.style.display === "none") {
+        c.style.display = "block";
+    } else {
+        c.style.display = "none";
+    }
+}
+
+function rock() {
+    var r = document.getElementById("myrock");
+    if (r.style.display === "none") {
+        r.style.display = "block";
+    } else {
+        r.style.display = "none";
     }
 }
