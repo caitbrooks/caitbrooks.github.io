afa568fcde0914dcf6c464a1abcb53348dec877a
Cait Brooks <cbrooks5@nd.edu>
2018-10-29 09:07:55 -0400

    project 2
    ocean css and html fixing and positioning

---
+++ project-2/assets/ocean.js
@@ -0,0 +1,42 @@
+$(document).ready(function (){
+    
+ $("#cloudbutton").click(function (){
+     $("#mycloud").toggleClass("visible");
+     $("#cloudbutton").toggleClass("green-button");
+ });       
+});
+
+$(document).ready(function (){
+        $("#sunbutton").click(function (){
+     $("#mysun").toggleClass("visible");
+     $("#sunbutton").toggleClass("green-button");
+ });  
+});
+
+$(document).ready(function (){
+        $("#wavebutton").click(function (){
+     $("#mywave").toggleClass("visible");
+     $("#wavebutton").toggleClass("green-button");
+ });  
+});
+
+$(document).ready(function (){
+        $("#plantbutton").click(function (){
+     $("#myplant").toggleClass("visible");
+     $("#plantbutton").toggleClass("green-button");
+ });  
+});
+
+
+$(document).ready(function (){
+        $("#castlebutton").click(function (){
+     $("#mycastle").toggleClass("visible");
+     $("#castlebutton").toggleClass("green-button");
+ });  
+});
+$(document).ready(function (){
+        $("#rockbutton").click(function (){
+     $("#myrock").toggleClass("visible");
+     $("#rockbutton").toggleClass("green-button");
+ });  
+});
